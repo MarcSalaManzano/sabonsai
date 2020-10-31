14baf1bb6a97d855f385ecdb59ebc817bd3f89df
MarcSalaManzano <marc.salam@e-campus.uab.cat>
2020-10-31 11:35:02 +0100

    index.php + footer view + header view

---
+++ view/footer.php
@@ -0,0 +1,10 @@
+<div>
+    <h2>Contact'ns</h2>
+    <p>lorem@ipsum.com</p>
+    <p>+34 111.111.111</p>
+</div>
+<div>
+    <a>Home</a>
+    <a>Log In</a>
+    <a>Sobre Nosaltres</a>
+</div>
---
+++ view/header.php
@@ -0,0 +1,5 @@
+<img class="logo" src=" <?php echo __DIR__.'/../media/logo.png' ?> " alt="Bonsai Tree Vectors by Vecteezy">
+<ul>
+    <li><a href="?login">Log in</a></li>
+    <li><a href="?carro" class="icon"><img src="media/cart-59-64.png"></a></li>
+</ul>
