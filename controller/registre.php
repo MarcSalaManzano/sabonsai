bae9f7dc697c5e4f9d34d7739662d7475a220ae8
Benhobels <Benhobels@users.noreply.github.com>
2020-11-13 18:04:37 +0100

    Prova per registre

--- controller/registre.php
+++ controller/registre.php
@@ -5,8 +5,10 @@ if(isset($_POST['submit'])) {
     require_once __DIR__ . '/../model/registre.php';
     $connexio = connectaBD();
 
-    if(comprovaRegistre($connexio))
+    if(comprovaRegistre($connexio)) {
         registrar($connexio);
+        require_once __DIR__ . '/../view/registreExit.html';
+    }
     else
         require_once __DIR__ . '/../view/registre.php';
 }
