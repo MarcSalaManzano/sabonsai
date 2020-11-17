b16cfc1f963541cbb3e5442c6245893570d60d45
MarcSalaManzano <marc.salam@e-campus.uab.cat>
2020-11-17 12:14:46 +0100

    Rework header

--- view/header.php
+++ view/header.php
@@ -1,4 +1,4 @@
-<img class="logo" src="/../media/logo.png" alt="Bonsai Tree Vectors by Vecteezy">
+<a href="index.php" class="logo"><img class="logo" src="/../media/logo.png" alt="Bonsai Tree Vectors by Vecteezy"></a>
 <ul>
     <li><a href="?accio=login">Log in</a></li>
     <li><a href="?accio=carro" class="icon"><img src="media/cart-59-64.png"></a></li>
