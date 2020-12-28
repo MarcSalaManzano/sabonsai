34cb3428dc138d7e4885bc8fea50a0c03531091e
MarcSalaManzano <marc.salam@e-campus.uab.cat>
2020-12-28 12:12:04 +0100

    Validacio xss

--- view/categories.php
+++ view/categories.php
@@ -1,5 +1,5 @@
 <ul id="nav-container">
     <?php foreach($llistatCategories as $categoria) { ?>
-        <li> <button class="navigation-button" id='<?php echo $categoria["ID"] ?>'> <?php echo $categoria['Name'] ?> </button></li>
+        <li> <button class="navigation-button" id='<?php echo $categoria["ID"] ?>'> <?php echo htmlentities($categoria['Name'], ENT_QUOTES | ENT_HTML5, 'UTF-8'); ?> </button></li>
     <?php } ?>
 </ul>
