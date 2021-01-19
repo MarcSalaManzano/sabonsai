82e465f7bc7c8589afa10bd1a5a63001d88eea38
MarcSalaManzano <marc.salam@e-campus.uab.cat>
2021-01-19 12:04:22 +0100

    No se la veritat

--- resource_carro.php
+++ resource_carro.php
@@ -4,18 +4,18 @@
     <meta name="viewport" content="initial-scale=1.0, width=device-width, user-scalable=yes"/>
     <link href="https://fonts.googleapis.com/css?family=Rubik:300,400,400i,700" rel="stylesheet"/>
     <link href="css/main.css" rel="stylesheet">
-    <link href="css/cabas.css">
+    <link href="css/cabas.css" rel="stylesheet">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
 
 </head>
 
 <body>
-<div id="layoutCarro">
+<div >
     <header id="header-container" style="grid-area: Head">
         <?php require __DIR__ . '/controller/header.php'; ?>
     </header>
 
-    <section>
+    <section id="layoutCarro">
         <?php require __DIR__ . '/controller/cabasPage.php'; ?>
     </section>
 
